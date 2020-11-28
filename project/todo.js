08fee11035d7ccd25b836e1c7692a7aef231c23b
rrkkee015 <45933358+rrkkee015@users.noreply.github.com>
2020-11-28 22:02:20 +0900

    Complete Chrome Momentum app without design

---
+++ project/todo.js
@@ -0,0 +1,71 @@
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  // 함수에 만족하는 것들만 받아서 새로 array를 만든다.
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  // localStorage엔 오직 string만 저장한다.
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+
+  li.id = newId;
+  delBtn.innerHTML = "❌";
+  delBtn.addEventListener("click", deleteToDo);
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedtoDos = localStorage.getItem(TODOS_LS);
+  if (loadedtoDos !== null) {
+    const parsedToDos = JSON.parse(loadedtoDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    })
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
